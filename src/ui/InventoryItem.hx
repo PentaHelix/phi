c14d6fd4597b91b7fdfd0d7da051a963903c56af
Jakob Kuen <jakkuen@gmail.com>
2019-09-06 22:28:26 +0200

    Moves InventoryItem into own class

---
+++ src/ui/InventoryItem.hx
@@ -0,0 +1,81 @@
+package ui;
+
+import hxd.Res;
+import items.Item;
+import hxd.Event;
+import h2d.Interactive;
+import items.Inventory;
+import h2d.Bitmap;
+import h3d.Vector;
+
+class InventoryItem extends Bitmap {
+  private var item: Item;
+  private var inventory: Inventory;
+
+  private var initialX: Float;
+  private var initialY: Float;
+  private var dragPos: Vector;
+  private var droppedSlot: Int = -1;
+
+  private var inter: Interactive;
+
+  private var slots: Array<Vector>;
+
+  public function new (item: Item, inventory: Inventory, slots: Array<Vector>) {
+    var tile = Res.load("items.png").toTile().sub(item.id * 16, 0, 16, 16).center();
+    super(tile);
+    this.item = item;
+    this.inventory = inventory;
+    this.slots = slots;
+
+    inter = new Interactive(14, 14, this);
+    inter.x = -7;
+    inter.y = -7;
+
+    initialX = x;
+    initialY = y;
+
+    inter.onPush = onPush;
+    inter.onRelease = onRelease;
+  }
+
+  private function onPush (ev: Event) {
+    initialX = x;
+    initialY = y;
+    dragPos = new Vector(initialX, initialY);
+    var offsetX: Float = ev.relX;
+    var offsetY: Float = ev.relY;
+    inter.startDrag(function (ev) {
+      dragPos.x += ev.relX - offsetX;
+      dragPos.y += ev.relY - offsetY;
+
+      x = dragPos.x;
+      y = dragPos.y;
+
+      droppedSlot = -1;
+      for (i in 0...slots.length) {
+        if (slots[i].distance(dragPos) < 4) {
+          droppedSlot = i;
+          x = slots[i].x;
+          y = slots[i].y;
+          dragPos.x = slots[i].x;
+          dragPos.y = slots[i].y;
+          break;
+        }
+      }
+    });
+  }
+
+  private function onRelease (ev: Event) {
+    if (droppedSlot != -1) {
+      inventory.move(item, droppedSlot);
+      x = slots[droppedSlot].x;
+      y = slots[droppedSlot].y;
+    } else {
+      x = initialX;
+      y = initialY;
+    }
+
+    inter.stopDrag();
+  }
+} 
--- src/ui/PlayerInventory.hx
+++ src/ui/PlayerInventory.hx
@@ -13,8 +13,7 @@ import items.Inventory;
 class PlayerInventory extends Object {
   private var inventory: Inventory;
   public var bmp: Bitmap;
-  private var itemBitmaps: Array<Bitmap> = [];
-  private var itemTiles: Array<Tile>;
+  private var itemBitmaps: Array<InventoryItem> = [];
 
   private var slots: Array<Vector> = [
     new Vector(16, 23),
@@ -32,14 +31,6 @@ class PlayerInventory extends Object {
     inventory = new Inventory(25);
     bmp = new Bitmap(Res.load('ui.png').toTile().sub(0, 0, 144, 57), this);
     y = 0;
-
-    var items = Res.load("items.png").toTile();
-    itemTiles = [
-			 for(x in 0 ... C.ITEM_COUNT)
-			 for(y in 0 ... 1)
-			 items.sub(x * 16, y * 16, 16, 16).center()
-		];
-    
   }
 
   override public function onAdd () {
@@ -57,60 +48,65 @@ class PlayerInventory extends Object {
     if (pos == -1) {
       // TODO
     } else {
-      var iBmp = new Bitmap(itemTiles[item.id]);
-      itemBitmaps.push(iBmp);
-      iBmp.x = slots[pos].x;
-      iBmp.y = slots[pos].y;
-      bmp.addChild(iBmp);
+      var invItem = new InventoryItem(item, inventory, slots);
+      invItem.x = slots[pos].x;
+      invItem.y = slots[pos].y;
+      bmp.addChild(invItem);
+      itemBitmaps.push(invItem);
+    //   var iBmp = new Bitmap(itemTiles[item.id]);
+    //   itemBitmaps.push(iBmp);
+    //   iBmp.x = slots[pos].x;
+    //   iBmp.y = slots[pos].y;
+    //   bmp.addChild(iBmp);
 
-      var interact = new Interactive(14, 14, iBmp);
-      interact.x = -7;
-      interact.y = -7;
+    //   var interact = new Interactive(14, 14, iBmp);
+    //   interact.x = -7;
+    //   interact.y = -7;
 
-      var initialX: Float = iBmp.x;
-      var initialY: Float = iBmp.y;
-      var dragPos: Vector;
-      var droppedSlot: Int = -1;
+    //   var initialX: Float = iBmp.x;
+    //   var initialY: Float = iBmp.y;
+    //   var dragPos: Vector;
+    //   var droppedSlot: Int = -1;
 
-      interact.onPush = function (ev) {
-        initialX = iBmp.x;
-        initialY = iBmp.y;
-        dragPos = new Vector(initialX, initialY);
-        var offsetX: Float = ev.relX;
-        var offsetY: Float = ev.relY;
-        interact.startDrag(function (ev) {
-          dragPos.x += ev.relX - offsetX;
-          dragPos.y += ev.relY - offsetY;
+    //   interact.onPush = function (ev) {
+    //     initialX = iBmp.x;
+    //     initialY = iBmp.y;
+    //     dragPos = new Vector(initialX, initialY);
+    //     var offsetX: Float = ev.relX;
+    //     var offsetY: Float = ev.relY;
+    //     interact.startDrag(function (ev) {
+    //       dragPos.x += ev.relX - offsetX;
+    //       dragPos.y += ev.relY - offsetY;
 
-          iBmp.x = dragPos.x;
-          iBmp.y = dragPos.y;
+    //       iBmp.x = dragPos.x;
+    //       iBmp.y = dragPos.y;
 
-          droppedSlot = -1;
-          for (i in 0...slots.length) {
-            if (slots[i].distance(dragPos) < 4) {
-              droppedSlot = i;
-              iBmp.x = slots[i].x;
-              iBmp.y = slots[i].y;
-              dragPos.x = slots[i].x;
-              dragPos.y = slots[i].y;
-              break;
-            }
-          }
-        });
-      };
+    //       droppedSlot = -1;
+    //       for (i in 0...slots.length) {
+    //         if (slots[i].distance(dragPos) < 4) {
+    //           droppedSlot = i;
+    //           iBmp.x = slots[i].x;
+    //           iBmp.y = slots[i].y;
+    //           dragPos.x = slots[i].x;
+    //           dragPos.y = slots[i].y;
+    //           break;
+    //         }
+    //       }
+    //     });
+    //   };
 
-      interact.onRelease = function (ev) {
-        if (droppedSlot != -1) {
-          inventory.move(item, droppedSlot);
-          iBmp.x = slots[droppedSlot].x;
-          iBmp.y = slots[droppedSlot].y;
-        } else {
-          iBmp.x = initialX;
-          iBmp.y = initialY;
-        }
+    //   interact.onRelease = function (ev) {
+    //     if (droppedSlot != -1) {
+    //       inventory.move(item, droppedSlot);
+    //       iBmp.x = slots[droppedSlot].x;
+    //       iBmp.y = slots[droppedSlot].y;
+    //     } else {
+    //       iBmp.x = initialX;
+    //       iBmp.y = initialY;
+    //     }
 
-        interact.stopDrag();
-      };
+    //     interact.stopDrag();
+    //   };
     }
   }
 }
